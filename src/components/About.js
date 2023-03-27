9bd7fcf16f165f17c4200571842665e15f964b4b
Pathmanaban Palsamy <pathmanaban.p@gmail.com>
2023-03-26 20:45:39 -0400

    Update responsive image with thumbnail

--- src/components/About.js
+++ src/components/About.js
@@ -2,31 +2,33 @@ import React, { Component } from 'react'
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/esm/Container';
 import Article from './Article';
+import Image from 'react-bootstrap/Image'
 export default class About extends Component {
     render() {
         return (
-            <div>
-                <Container>
-                    <div >
-                        <Container>
-                            <Card style={{ width: '48rem', height: '4rem' }}>
-                                <Card.Body>
-                                    <Card.Header>Pathmanaban Palsamy</Card.Header>
-                                    <Card.Text>
-                                        <Article name='about' />
-                                    </Card.Text>
 
-                                </Card.Body>
-                                <Card.Footer style={{}}>
-                                    <Card.Link href="https://www.linkedin.com/in/pathmanabanp/" target={'_blank'}>Linkedin</Card.Link>
-                                    <Card.Link href="https://github.com/ppalsamy" target={'_blank'}>GitHub</Card.Link>
-                                </Card.Footer>
-                            </Card>
-                        </Container>
-                    </div>
+            <Container>
+
+                <Container>
+                    <Card border='light' style={{ width: '90%', height: '4rem' }}>
+                        <Card.Header>Pathmanaban Palsamy</Card.Header>
+                        <Card.Body >
+                            <Image src='/img/path.jpeg' roundedCircle />
+                            <Card.Text>
+                                <Article name='about' />
+                            </Card.Text>
+
+                        </Card.Body>
+                        <Card.Footer style={{}}>
+                            <Card.Link href="https://www.linkedin.com/in/pathmanabanp/" target={'_blank'}>Linkedin</Card.Link>
+                            <Card.Link href="https://github.com/ppalsamy" target={'_blank'}>GitHub</Card.Link>
+                        </Card.Footer>
+                    </Card>
                 </Container>
 
-            </div>
+            </Container>
+
+
         );
     }
 }
